4f88af50030d11350e0f074c38d983e393aeac44
js3322 <js@cleancode.kr>
2023-10-31 20:52:31 +0900

    FEAT #0 init

---
+++ src/main/java/com/cleancode/monitoring_gateway/MonitoringGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cleancode.monitoring_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cleancode/monitoring_gateway/MonitoringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancode.monitoring_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
